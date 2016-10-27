60b979c96d8e6813054b30b3c999c28c5c60e754
HieuNM6 <hieunmce@gmail.com>
2016-10-27 20:01:09 +0700

    milstone 2

--- app/routes.js
+++ app/routes.js
@@ -7,17 +7,25 @@ module.exports = (app) => {
     res.render('index.ejs', {message: req.flash('error')})
   })
 
-  app.post('/login', passport.authenticate('local', {
+  app.post('/login', passport.authenticate('local-login', {
     successRedirect: '/profile',
     failureRedirect: '/',
     failureFlash: true
   }))
+  
+  app.get('/login', (req, res) => {
+      res.render('login.ejs', {message: req.flash('error')})
+  })
   // process the signup form
   app.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/profile',
     failureRedirect: '/',
     failureFlash: true
   }))
+  
+  app.get('/signup', (req, res) => {
+      res.render('signup.ejs', {message: req.flash('error') })
+  })
 
   app.get('/profile', isLoggedIn, (req, res) => {
     res.render('profile.ejs', {
